bf9a777586359bc7baf513c70bc094ae41a971b5
william292 <william@table4.ai>
2024-12-24 15:39:50 -0500

    harris christmas

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,9 @@ import Snowfall from 'react-snowfall'
 import Card from './components/Card'
 import Snowman from './components/Snowman'
 import './App.css'
-import evaNYC from './assets/eva-nyc.jpg';
-import lizImage from './assets/liz.jpeg';
-import fionaImage from './assets/fiona.jpeg';
-import finnImage from './assets/finn.jpeg';
-import ianImage from './assets/ian.jpeg';
-import kathyImage from './assets/kathy.jpeg';
-import angelImage from './assets/angel.jpeg';
-import beccaImage from './assets/becca.jpeg';
-import benImage from './assets/ben.jpeg';
-import dadImage from './assets/dad.jpeg';
+import momImage from './assets/mom.jpeg'
+import jeffImage from './assets/jeff.jpeg'
+import wellyImage from './assets/welly.jpeg'
 
 function App() {
   const [scrollY, setScrollY] = useState(0);
@@ -21,74 +14,25 @@ function App() {
 
   const cards = [
     {
-      name: "Eva",
-      image: evaNYC,
-      message: "You have a way of holding our family together, always knowing when someone needs a kind word or a listening ear. Your empathy and unwavering support mean so much to me, and I’m grateful for the love and stability you bring to all of us. Thank you for always being there.",
-      signoff: "Love,",
-      signature: "The Boy"
-    },
-    {
-      name: "Liz",
-      image: lizImage,
-      message: "You’re more than my sister—you’re my confidant, my sounding board, and my best friend. I don’t know where I’d be without our daily chats and the way you’re always there to celebrate the wins and get through the losses. You make life better just by being you.",
-      signoff: "With love,",
-      signature: "William"
-    },
-    {
-      name: "Fiona",
-      image: fionaImage,
-      message: "You bring so much joy with your curiosity and adventurous spirit. Whether we’re at the beach or the pool, your energy and kindness remind me to enjoy life’s little moments. I can’t wait for our next adventure together!",
-      signoff: 'See you at the beach!',
-      signature: "Beach Buddy"
-    },
-    {
-      name: "Finn",
-      image: finnImage,
-      message: "You’re such a fun and fearless kid, always full of energy and curiosity. I love seeing you light up the room with your enthusiasm and how much you adore your sister. Even though we don’t see each other as much as I’d like, I’m so proud of the awesome person you’re becoming.",
-      signoff: 'Your amazing uncle,',
-      signature: "William"
-    },
-    {
-      name: "Ian",
-      image: ianImage,
-      message: "Your passion for cooking and hosting makes every visit feel special. Whether you’re grilling or watching the game, you make everyone feel welcome and at ease.",
-      signoff: 'Have a Merry Christmas,',
-      signature: "William"
-    },
-    {
-      name: "Kathy",
-      image: kathyImage,
-      message: "You’ve had such a big year, and I’m amazed by your resilience and drive. Balancing residency and everything life has thrown your way, you handle it all with strength and grace. I can’t wait to see you start this next chapter of your career—it’s so well-deserved.",
+      name: "Mom",
+      image: momImage,
+      message: "Of everyone in the family, you show me the most love and empathy without judging me.  I appreciate you calling me, visiting me and treating me like a part of your life.  I love you so much and wish you a very Merry Christmas.",
       signoff: "Merry Christmas!",
-      signature: "Your Brother"
-    },
-    {
-      name: "Angel",
-      image: angelImage,
-      message: "You’ve grown into such a witty and kind young man. I love our banter and the way you’ve embraced Dad’s humor (for better or worse). Keep being the thoughtful and fun person you are. You’ve got an incredible future ahead!",
-      signoff: "Merry Christmas!",
-      signature: "William"
-    },
-    {
-      name: "Becca",
-      image: beccaImage,
-      message: "You bring so much laughter and fun to our family. I’ll miss playing board games with you this year, but don’t worry—we all know I’d have won. 😉 Your easygoing and honest approach to life is something I truly admire.",
-      signoff: "Merry Christmas!",
-      signature: "William"
-    },
-    {
-      name: "Ben",
-      image: benImage,
-      message: "It’s been great getting to know you and sharing our love for music and stories. You bring such a calming and welcoming presence to every room. I’m glad to have you as part of the family.",
-      signoff: "Happy Holidays!",
-      signature: "William"
-    },
-    {
-      name: 'Dad',
-      image: dadImage,
-      message: "Your support and wisdom have guided me through so much. Whether it’s one-word texts or hours-long calls, I know you’re always there for me. I look up to your dedication and drive, but don’t forget to take time for yourself—you’ve earned it.",
-      signoff: 'With love,',
       signature: "Your Son"
+    },
+    {
+      name: "Jeff",
+      image: jeffImage,
+      message: "I know you're just ready to walk out of that job (and moon them on the way out).  Soon you'll be up in north GA building decks and mending fences.  I love you and miss you - I hope you have a jolly holly Christmas.  Don't drink and drive.",
+      signoff: "With love,",
+      signature: "The Grill Master"
+    },
+    {
+      name: "Welly Belly",
+      image: wellyImage,
+      message: "I miss you, ya big goof troop.  Tell momma you're sad that she killed the pool.  Be kind this Christmas and don't eat the cat.",
+      signoff: 'Woof, woof, woof,',
+      signature: "The One who named you + Beurre"
     }
 ];
 
@@ -153,11 +97,11 @@ function App() {
       <div className="header-container">
         <div className="header-content">
           <h1>
-            Merry Christmas, Family and Friends!
+            Merry Christmas, Y'all!
             <span className="christmas-tree">🎄</span>
           </h1>
           <p className="intro-message">
-            While you're all bundled up in the winter wonderland, I'm sending warm tropical wishes your way! 
+            While you're hanging out in the sticks, I'm sending warm tropical wishes your way! 
             Your love and support mean the world to me, and even though I'm enjoying palm trees and frozen margaritas, 
             my heart is with you this holiday season. Each of you brings something special to my life, and I'm 
             grateful for the joy, laughter, and memories we share.
